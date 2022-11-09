c7426e1495d50a166ad46112fcd7d051bc6cb179
Sean Dylan Patterson <74496368+SeanDylan1982@users.noreply.github.com>
2022-11-09 14:50:02 +0200

    Add files via upload

---
+++ precache-manifest.a57e5679f087f9942d11481c51c89e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a5ebf0a31b688181b15",
+    "url": "/static/css/main.54e4938c.chunk.css"
+  },
+  {
+    "revision": "3a5ebf0a31b688181b15",
+    "url": "/static/js/main.3a5ebf0a.chunk.js"
+  },
+  {
+    "revision": "5f24b6ae34aadc2b40f8",
+    "url": "/static/css/1.5ce918d1.chunk.css"
+  },
+  {
+    "revision": "5f24b6ae34aadc2b40f8",
+    "url": "/static/js/1.5f24b6ae.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "674f0cf09e767cdf0ffd1a6f430f9f8d",
+    "url": "/index.html"
+  }
+];
